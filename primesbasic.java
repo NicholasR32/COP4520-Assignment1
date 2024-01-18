426836e33cbf1fdbb1f80c8ac9f8b462bca9f48b
Nicholas Rolland <nicholasrolland32@gmail.com>
2024-01-18 00:59:16 -0500

    Upload files

---
+++ primesbasic.java
@@ -0,0 +1,13 @@
+// Nicholas Rolland
+// Assignment 1
+// COP4520, Spring 2024, Prof. Juan Parra
+// 1/26/2024
+
+
+public class primesbasic
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
